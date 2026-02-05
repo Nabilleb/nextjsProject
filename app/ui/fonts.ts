dbcde489550b667160a6acb5509b5ad8a076b6eb
Nabilleb <nabilgreen500@gmail.com>
2026-02-05 14:45:37 +0200

    fixing folder structure and design

---
+++ app/ui/fonts.ts
@@ -0,0 +1,17 @@
+import exp from 'constants';
+import {Inter, Lusitana,Roboto} from 'next/font/google';
+
+export const inter = Inter({
+    subsets: ['latin'],
+    weight: ['400', '700'],
+});
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
+
+export const roboto = Roboto({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
